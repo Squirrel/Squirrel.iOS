65a3bd5871ba113397090ff31c268a3adfc3ad17
Robert Böhnke <robb@robb.is>
2014-10-28 14:26:27 +0100

    Add Xcode project

---
+++ Squirrel/Squirrel.h
@@ -0,0 +1,19 @@
+//
+//  Squirrel.h
+//  Squirrel
+//
+//  Created by Robert Böhnke on 28/10/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Squirrel.
+FOUNDATION_EXPORT double SquirrelVersionNumber;
+
+//! Project version string for Squirrel.
+FOUNDATION_EXPORT const unsigned char SquirrelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Squirrel/PublicHeader.h>
+
+
